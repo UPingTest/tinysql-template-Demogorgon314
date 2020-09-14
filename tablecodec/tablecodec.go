97c2e9b53e134b70248de95d8603d43cce011f5e
Demogorgon314 <wangkai744567028@gmail.com>
2020-09-14 15:14:52 +0800

    完成 EncodeIndexSeekKey 和 DecodeRecordKey

--- tablecodec/tablecodec.go
+++ tablecodec/tablecodec.go
@@ -72,7 +72,21 @@ func EncodeRowKeyWithHandle(tableID int64, handle int64) kv.Key {
 // DecodeRecordKey decodes the key and gets the tableID, handle.
 func DecodeRecordKey(key kv.Key) (tableID int64, handle int64, err error) {
 	/* Your code here */
-	return
+	if key == nil {
+		return 0, 0, errors.New("key is nil")
+	}
+	if !hasTablePrefix(key) {
+		return 0, 0, errors.New("key verification failed")
+	}
+	key, tableID, err = codec.DecodeInt(key[len(tablePrefix):])
+	if err != nil {
+		return 0, 0, errors.Trace(err)
+	}
+	key, handle, err = codec.DecodeInt(key[len(recordPrefixSep):])
+	if err != nil {
+		return 0, 0, errors.Trace(err)
+	}
+	return tableID, handle, nil
 }
 
 // appendTableIndexPrefix appends table index prefix  "t[tableID]_i".
@@ -95,6 +109,23 @@ func EncodeIndexSeekKey(tableID int64, idxID int64, encodedValue []byte) kv.Key 
 // DecodeIndexKeyPrefix decodes the key and gets the tableID, indexID, indexValues.
 func DecodeIndexKeyPrefix(key kv.Key) (tableID int64, indexID int64, indexValues []byte, err error) {
 	/* Your code here */
+	if key == nil {
+		return 0, 0, nil, errors.New("key is nil")
+	}
+	if !hasTablePrefix(key) {
+		return 0, 0, nil, errors.New("key verification failed")
+	}
+	key, tableID, err = codec.DecodeInt(key[len(tablePrefix):])
+	if err != nil {
+		return 0, 0, nil, errors.Trace(err)
+	}
+	if !hasIndexPrefixSep(key) {
+		return 0, 0, nil, errors.New("key verification failed")
+	}
+	indexValues, indexID, err = codec.DecodeInt(key[len(indexPrefixSep):])
+	if err != nil {
+		return 0, 0, nil, errors.Trace(err)
+	}
 	return tableID, indexID, indexValues, nil
 }
 
@@ -160,6 +191,10 @@ func hasTablePrefix(key kv.Key) bool {
 
 func hasRecordPrefixSep(key kv.Key) bool {
 	return key[0] == recordPrefixSep[0] && key[1] == recordPrefixSep[1]
+}
+
+func hasIndexPrefixSep(key kv.Key) bool {
+	return key[0] == recordPrefixSep[0] && key[1] == indexPrefixSep[1]
 }
 
 // DecodeMetaKey decodes the key and get the meta key and meta field.
